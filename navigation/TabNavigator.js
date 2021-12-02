b7ec8d7ab94ffc5765a47a9340d831864ef683ab
Aastha <xyz@gmail.com>
2021-12-02 11:43:32 +0530

    ex

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Feed from '../screens/Feed';
 import CreateStory from '../screens/CreateStory';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-const Tab=createBottomTabNavigator();
+const Tab=createMaterialBottomTabNavigator();
 
 const BottomTabNavigator=()=> {
   return (
 
      <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: 'red',
-        tabBarInactiveTintColor:'grey',
-        headerShown:false
-      }}
-     
+        
+         labeled={false}
+         activeColor='orange'
+         barStyle={styles.bottomTabStyle}
      >
        <Tab.Screen name="Feed" component={Feed} 
         options={{
   
-          tabBarIcon: ({}) => (
-            <Ionicons name="book-outline"></Ionicons>
-         
+          tabBarIcon: ({color}) => (
+          
+            <Ionicons name="book-outline" color={color} size={30} style={styles.icons}></Ionicons>
+            
           ),
+
         }}/>
-       <Tab.Screen name="CreateStory" component={CreateStory}
+       <Tab.Screen name="Create Story" component={CreateStory}
         options={{
          
-          tabBarIcon: ({}) => (
-            <Ionicons name="clipboard-outline"></Ionicons>
+          tabBarIcon: ({color}) => (
+            <Ionicons name="clipboard-outline" color={color} size={30} style={styles.icons}></Ionicons>
           )
         }}
        />
@@ -39,4 +40,20 @@ const BottomTabNavigator=()=> {
   
   );
 }
+
+const styles=StyleSheet.create({
+  bottomTabStyle:{
+    backgroundColor:'#002147',
+    height:'8%',
+    borderTopLeftRadius:30,
+    borderTopRightRadius:30,
+    position:'absolute',
+    overflow:'hidden'
+    
+  },
+  icons:{
+    width:RFValue(30),
+    height:RFValue(30)
+  }
+})
 export default BottomTabNavigator;
